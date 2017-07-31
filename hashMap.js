f8df70ccf8cdc57f19133181ccd28b6330f71757
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-07-31 08:22:19 -0700

    finish constructor and hash function

--- hashMap.js
+++ hashMap.js
@@ -1,5 +1,21 @@
 'use strict';
 
 class HashMap {
+  constructor(initialCapacity = 8){
+    this._capacity = initialCapacity;
+    this.length = 0;
+    this._slots = [];
+    this.MAX_LOAD_RATIO = 0.9;
+    this.SIZE_RATIO = 3;
+  }
 
+  static _hashString(string) {
+    let hash = 5381;
+    for (let i=0; i<string.length; i++) {
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      hash = hash & hash;
+    }
+    return hash >>> 0;
+  }
 }
+
